8d7152439b2d15b462ffb3e2b3c4ede0cbe567b5
srushtimane27 <srushtimane31@gmail.com>
2022-06-20 22:02:07 +0530

    program

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+public class Practice {
+    public static void main(String[] args) {
+
+        int x = 40;
+        int y = 20;
+
+        if (x>y) {
+            System.out.println("x is greater than y");
+        } else {
+            System.out.println("y is greater than x ");
+        }
+    }
+}
